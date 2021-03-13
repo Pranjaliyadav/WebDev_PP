1a48549042b75e104cc471cee8c9f22c8f7f5c07
Pranjali <yadavpranjali1233@gmail.com>
2021-03-14 03:26:49 +0530

    web scrapping

---
+++ Lec5_WebScrapping/facts/fileRead.js
@@ -0,0 +1,36 @@
+/*const fs = require("fs");
+const cheerio = require("cheerio");
+
+let htmlKaData = fs.readFileSync("./index.html");
+
+let ch = cheerio.load(htmlKaData);
+
+let pTag = ch(".main");
+
+console.log(pTag);*/
+const fs = require("fs");
+const cheerio = require("cheerio");
+
+
+let htmlKaData = fs.readFileSync("./index.html");
+// htmlKaData => html treat to
+
+let ch = cheerio.load(htmlKaData);
+
+// console.log(ch);
+
+let pTags = ch("p");
+console.log(pTags);
+
+
+
+//<p class="main">I am a p tag in body !!!</p> => object form
+
+
+
+
+
+
+
+
+// html me se => selector ke base => element get
