027c32e0bbc267f69bda8020faf10a24a4c88cb1
Pranjali <yadavpranjali1233@gmail.com>
2021-04-04 17:58:44 +0530

    hehe

--- Lec10_awaitAsync/activity/MyaddChallenges.js
+++ Lec10_awaitAsync/activity/MyaddChallenges.js
@@ -1,68 +1,58 @@
-const puppeteer = require("puppeteer");
+let puppeteer = require("puppeteer");
 const id = "geetkhatri9@gmail.com";
 const pw = "poiuytrewq123";
-
 
 let challenges = require("./challenges");
 
 (async function(){
     let browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        args: ["--start-maximized"],
-      }); // 10 sec
+        headless : false,
+        defaultViewport : null,
+        args : ["--start-maximized"],
+    });
     let allPages = await browser.pages();
     let tab = allPages[0];
     await tab.goto("https://www.hackerrank.com/auth/login");
     await tab.type("#input-1", id);
     await tab.type("#input-2", pw);
     await tab.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled");
-    await tab.waitForSelector('div[data-analytics="NavBarProfileDropDown"]' , {visible:true});
+    await tab.waitForSelector('div [data-analytics="NavBarProfileDropDown"]', {visible : true});
     await tab.click('div[data-analytics="NavBarProfileDropDown"]');
-    await tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]' , {visible:true});
+    await tab.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]', {visible : true});
     await tab.click('a[data-analytics="NavBarProfileDropDownAdministration"]');
-    // delay
+    //as delay is happening we'll use tab.waitForTimeout(5000);
     await tab.waitForTimeout(5000);
     await tab.waitForSelector('.nav-tabs.nav.admin-tabbed-nav a' , {visible:true});
     let bothATags = await tab.$$('.nav-tabs.nav.admin-tabbed-nav a');
     let manageChallengeATag = bothATags[1];
     await manageChallengeATag.click();
+    await tab.waitForSelector('.btn.btn-green.backbone.pull-right' , {visible:true})
+    let createChallengeButton = await tab.$('.btn.btn-green.backbone.pull-right')
+    let createChallengeLink = await tab.evaluate(function(elem){return elem.getAttribute("href"); } , createChallengeButton);
+    createChallengeLink = 'https://www.hackerrank.com' + createChallengeLink;
 
-    await tab.waitForSelector('.btn.btn-green.backbone.pull-right' , {visible:true});
-    let createChallengeBtn = await tab.$('.btn.btn-green.backbone.pull-right');
-    // console.log(createChallengeBtn);
-    let createChallengeLink = await tab.evaluate( function(elem){ return elem.getAttribute("href");  }  ,  createChallengeBtn);
-    createChallengeLink = 'https://www.hackerrank.com'+createChallengeLink;
-
-    // simultaenously open tabs for all the challenges
-    for(let i=0 ; i<challenges.length ; i++){
-        addChallenge(challenges[i] , browser , createChallengeLink );
-        await tab.waitForTimeout(3000);
-    }
+    //await addChallenge(challenge[0], browser , createChallengeLink);
     
-    // OR
+    //for(let i=0 ; i<challenges.length ; i++){
+      //  addChallenge(challenges[i] , browser , createChallengeLink );
+       // await tab.waitForTimeout(3000);
+    //}
+    
 
-    // add challenges one by one
-    // for(let i=0 ; i<challenges.length ; i++){
-    //     // add a single challenge
-    //     await addChallenge(challenges[i] , browser , createChallengeLink );
-    // }
-    // await addChallenge(challenges[0] , browser , createChallengeLink);
-})();   
+    for(let i=0 ; i<challenges.length ; i++){
+             // add a single challenge
+            await addChallenge(challenges[i] , browser , createChallengeLink );
+        }
+    
+    
 
 
+    
 
-// by default returns a pending promise
-async function addChallenge(challenge , browser , createChallengeLink ){
-    // {
-    //     "Challenge Name": "Pep_Java_1GettingStarted_1IsPrime",
-    //     "Description": "Question 1",
-    //     "Problem Statement": "Take as input a number n. Determine whether it is prime or not. If it is prime, print 'Prime' otherwise print 'Not Prime.",
-    //     "Input Format": "Integer",
-    //     "Constraints": "n <= 10 ^ 9",
-    //     "Output Format": "String",
-    //     "Tags": ["Basics" , "Array" , "Getting_Started"]
-    //   }
+
+})();  //IIFE
+
+async function addChallenge(challenge , browser , createChallengeLink){
 
     let challengeName = challenge["Challenge Name"];
     let description = challenge["Description"];
@@ -85,4 +75,5 @@ async function addChallenge(challenge , browser , createChallengeLink ){
     await newTab.keyboard.press("Enter");
     await newTab.click('.save-challenge.btn.btn-green');
     await newTab.close();
+
 }
