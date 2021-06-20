91541a2c7beab02c983a2dcedb0684fe4298adf7
Pranjali <yadavpranjali1233@gmail.com>
2021-06-20 14:02:24 +0530

    movies data render

---
+++ Module3_React/movies/src/API/secrets.js
@@ -0,0 +1,3 @@
+export const API_URL = "https://api.themoviedb.org/3";
+export const API_KEY = "c4f79a656174a40d7584098be8efeb0e";
+export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
--- Module3_React/movies/src/App.js
+++ Module3_React/movies/src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
+import Header from './Components/Header/Header';
+import Movies from './Components/Movies/Movies.jsx';
+import axios from "axios";
+import {API_URL , API_KEY , IMAGE_URL} from "./API/secrets";
 
 class App extends Component {
-  state = {  }
+  state = {
+    moviesData : [],
+    currentMovie:"mission"
+    };
+
+  async componentDidMount(){
+    //API call
+    //using axios -> npm i axios
+    //parameter-> api key, page , query
+    //https://api.themoviedb.org/3/search/movie?api_key=c4f79a656174a40d7584098be8efeb0e&query=mission&page=1&include_adult=false
+    
+    let data = await axios.get( API_URL + "/search/movie" , 
+        {params : {api_key:API_KEY, page: 1,query: this.state.currentMovie }
+    });
+    let moviesData = data.data.results;
+    
+    this.setState({
+      moviesData:moviesData
+    })
+    
+
+  }
+
   render() { 
     return (  <div className="App">
-    Hello from App
-  </div> );
+      <Header></Header>
+      <Movies movies = {this.state.moviesData}></Movies>
+    </div> );
   }
 }
  
