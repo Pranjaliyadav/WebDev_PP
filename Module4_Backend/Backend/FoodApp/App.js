b8273db20eee37dd37277730d42cb570abf73cdc
Pranjali <yadavpranjali1233@gmail.com>
2021-09-18 21:45:50 +0530

    backend-1

---
+++ Module4_Backend/Backend/FoodApp/App.js
@@ -0,0 +1,48 @@
+const express = require("express");
+//express is abstraction over node.js for writing backend
+
+const app = express();
+//initializing a server
+app.use(express.json());
+//post accept geeting the data for req
+
+app.get("/",function(req,res){
+console.log("hello from home page");
+res.send("<h1>Hey from backend</h1>");
+
+})
+//frontend - req /
+//function -> route path
+
+let user = {
+
+};
+
+app.get("/user", function(req,res){
+    console.log("users");
+    res.json(user); //as its an object key value pair so using json
+})
+
+app.post("/user",function(req,res){
+    
+    console.log("req.data",req.body);
+    user = req.body;
+    res.status(200).send("data received and user updated");
+})
+//updating info
+app.patch("/user",function(req,res){
+    let obj = req.body;
+    for(let key in obj){
+        user[key] = obj[key];
+    }
+    res.status(200).json(user); //print user on postman
+})
+
+app.delete("/user",function(req,res){
+    user = {};
+    res.status(200).json(user);
+})
+//localhost:8080 ??
+app.listen(3000,function(){
+console.log("server started");
+})
