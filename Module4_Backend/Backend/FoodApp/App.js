34f82ec6d4f94570d734dde4b42734da71bd52e8
Pranjali <yadavpranjali1233@gmail.com>
2021-09-18 22:15:55 +0530

    backend-1

--- Module4_Backend/Backend/FoodApp/App.js
+++ Module4_Backend/Backend/FoodApp/App.js
@@ -42,6 +42,12 @@ app.delete("/user",function(req,res){
     user = {};
     res.status(200).json(user);
 })
+
+//template routes
+app.get("/user/:id",function(req,res){
+    console.log(req.params,req.params.id);
+    res.status(200).send("Hello");
+})
 //localhost:8080 ??
 app.listen(3000,function(){
 console.log("server started");
